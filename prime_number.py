a0498885fa2e2340325d8a3c4115bb19e68644f8
Monalipatnaik <98518832+Monalipatnaik@users.noreply.github.com>
2022-08-26 10:04:58 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) if prime(n)==True:     print("prime") else:     print("not a prime")

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+if prime(n)==True:
+    print("prime")
+else:
+    print("not a prime")
